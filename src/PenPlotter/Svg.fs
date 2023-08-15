1b8d965fc479ac07d546ca89b04aa63497960afb
Thomas Waters <twaters117@gmail.com>
2023-08-14 21:48:36 -0400

    Circle2D geometry creation

---
+++ src/PenPlotter/Svg.fs
@@ -0,0 +1,43 @@
+﻿module PenPlotter.Svg
+
+open SharpVG
+open Math.Geometry
+open Math.Units
+
+open PenPlotter
+
+/// The unit conversion from the typed Length to untyped float used in the Svg coordinate system
+let unwrapLength (len: Math.Units.Length) : float = Length.inCssPixels len
+
+let toLength (len: Math.Units.Length) : SharpVG.Length =
+    unwrapLength len
+    |> SharpVG.Length.ofFloat
+
+/// Create an Svg style based on the pen that is being used
+let styleFromPen (pen: PenPlotter.Pen) : SharpVG.Style =
+    let strokeWidth = toLength pen.Thickness
+    let svgPen = SharpVG.Pen.createWithWidth (Color.ofName Colors.Black) strokeWidth
+    Style.empty |> Style.withStrokePen svgPen
+    
+let toPoint (point: Point2D<Meters, 'Coordinates>) : Point =
+    Point.ofFloats (unwrapLength point.X, unwrapLength point.Y)
+
+/// Create an SVG based on a pen and the input geometry.
+/// This needs to be used with the Cartesian coordinate system.
+let fromGeometry (pen: PenPlotter.Pen) (geometry: IGeometry) : Element =
+    let style = styleFromPen pen
+    
+    match geometry with
+    | :? Line2D<Meters, Cartesian> as line ->
+        let start = toPoint line.Start
+        let finish = toPoint line.Finish
+        
+        Line.create start finish |> Element.createWithStyle style
+        
+    | :? Circle2D<Meters, Cartesian> as circle ->
+        let center = toPoint circle.Center
+        let radius = toLength circle.Radius
+        
+        Circle.create center radius |> Element.createWithStyle style
+
+    | _ -> failwith $"Unable to create SVG from geometry:\n{geometry}"
--- test/PenPlotter.Tests/Plotter.fs
+++ test/PenPlotter.Tests/Plotter.fs
@@ -2,28 +2,38 @@ module PenPlotter.Tests.Plotter
 
 open NUnit.Framework
 
-open PenPlotter
 open Math.Units
 open Math.Geometry
+open SharpVG
+
+open PenPlotter
 
 [<SetUp>]
 let Setup () = ()
+
+
+let testGeometry (geom: IGeometry) (expected: string) : unit =
+    let pen = Pen.create [ Pen.thickness (Length.cssPixels 2); Pen.color "#000000" ]
+    let elementToString (ele: Element) : string = $"{ele}"
+    let actual = Svg.fromGeometry pen geom |> elementToString
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Circle2D to SVG`` () =
+    let expected: string =
+        """<circle stroke="black" stroke-width="2" opacity="1" r="25" cx="50" cy="100"/>"""
+
+    let geom: Circle2D<Meters, Cartesian> =
+        Circle2D.atPoint (Point2D.pixels 50. 100.) (Length.cssPixels 25.)
+
+    testGeometry geom expected
 
 [<Test>]
 let ``Line2D to SVG`` () =
     let expected: string =
         """<line stroke="black" stroke-width="2" opacity="1" x1="0" y1="100" x2="100" y2="100"/>"""
-    
 
-    let line: Line2D<Meters, Cartesian> =
-        Line2D.through
-            (Point2D.pixels 0. 100.)
-            (Point2D.pixels 100. 100.)
+    let geom: Line2D<Meters, Cartesian> =
+        Line2D.through (Point2D.pixels 0. 100.) (Point2D.pixels 100. 100.)
 
-    let black = Pen.create [ Pen.thickness (Length.cssPixels 2); Pen.color "#000000" ]
-
-    let svg: string =
-        Plotter.Svg.fromGeometry black line
-        |> sprintf "%O"
-
-    Assert.AreEqual(expected,  svg)
+    testGeometry geom expected
