382c6eed48b48edc53df46b512ba915f8a1c00f6
Thomas Waters <twaters117@gmail.com>
2023-08-15 09:07:29 -0400

    Rectangle, Bounding Box, and Polygon Svg conversions

--- src/PenPlotter/Svg.fs
+++ src/PenPlotter/Svg.fs
@@ -1,4 +1,5 @@
-﻿module PenPlotter.Svg
+﻿/// Convert Math.Geometry objects into Svg files.
+module PenPlotter.Svg
 
 open SharpVG
 open Math.Geometry
@@ -6,38 +7,69 @@ open Math.Units
 
 open PenPlotter
 
-/// The unit conversion from the typed Length to untyped float used in the Svg coordinate system
-let unwrapLength (len: Math.Units.Length) : float = Length.inCssPixels len
 
-let toLength (len: Math.Units.Length) : SharpVG.Length =
-    unwrapLength len
-    |> SharpVG.Length.ofFloat
+module GeometryToSharpVG =
+    /// The unit conversion from the typed Length to untyped float used in the Svg coordinate system
+    let unwrapLength (len: Math.Units.Length) : float = Length.inCssPixels len
+
+    let toLength (len: Math.Units.Length) : SharpVG.Length =
+        unwrapLength len |> SharpVG.Length.ofFloat
+
+    let toPoint (point: Point2D<Meters, 'Coordinates>) : SharpVG.Point =
+        Point.ofFloats (unwrapLength point.X, unwrapLength point.Y)
+
+    let toLine (line: Line2D<Meters, 'Coordinates>) : SharpVG.Line =
+        let start = toPoint line.Start
+        let finish = toPoint line.Finish
+        Line.create start finish
+
+    let toCircle (circle: Circle2D<Meters, 'Coordinates>) : SharpVG.Circle =
+        let center = toPoint circle.Center
+        let radius = toLength circle.Radius
+        Circle.create center radius
+
+    let toArea (size: Size2D<Meters>) : SharpVG.Area =
+        Area.ofFloats (unwrapLength size.Width, unwrapLength size.Height)
+
+    let toRectangle (rectangle: Rectangle2D<Meters, 'Coordinates>) : SharpVG.Rect =
+        let anchorPoint = List.head (Rectangle2D.vertices rectangle) |> toPoint
+        let dimensions = Rectangle2D.dimensions rectangle |> toArea
+        Rect.create anchorPoint dimensions
+
+    /// TODO: take care of inner loop geometry
+    let toPolygon (polygon: Polygon2D<Meters, 'Coordinates>) : SharpVG.Polygon =
+        List.map toPoint (Polygon2D.outerLoop polygon) |> Polygon.ofSeq
+
+
+    let bboxToRectangle (bbox: BoundingBox2D<Meters, 'Coordinates>) : SharpVG.Rect =
+        Rectangle2D.fromBoundingBox bbox |> toRectangle
 
 /// Create an Svg style based on the pen that is being used
 let styleFromPen (pen: PenPlotter.Pen) : SharpVG.Style =
-    let strokeWidth = toLength pen.Thickness
+    let strokeWidth = GeometryToSharpVG.toLength pen.Thickness
     let svgPen = SharpVG.Pen.createWithWidth (Color.ofName Colors.Black) strokeWidth
     Style.empty |> Style.withStrokePen svgPen
-    
-let toPoint (point: Point2D<Meters, 'Coordinates>) : Point =
-    Point.ofFloats (unwrapLength point.X, unwrapLength point.Y)
 
 /// Create an SVG based on a pen and the input geometry.
 /// This needs to be used with the Cartesian coordinate system.
 let fromGeometry (pen: PenPlotter.Pen) (geometry: IGeometry) : Element =
     let style = styleFromPen pen
-    
-    match geometry with
-    | :? Line2D<Meters, Cartesian> as line ->
-        let start = toPoint line.Start
-        let finish = toPoint line.Finish
-        
-        Line.create start finish |> Element.createWithStyle style
-        
-    | :? Circle2D<Meters, Cartesian> as circle ->
-        let center = toPoint circle.Center
-        let radius = toLength circle.Radius
-        
-        Circle.create center radius |> Element.createWithStyle style
 
-    | _ -> failwith $"Unable to create SVG from geometry:\n{geometry}"
+    match geometry with
+    | :? Line2D<Meters, Cartesian> as line -> GeometryToSharpVG.toLine line |> Element.createWithStyle style
+
+    | :? Circle2D<Meters, Cartesian> as circle -> GeometryToSharpVG.toCircle circle |> Element.createWithStyle style
+
+    | :? Rectangle2D<Meters, Cartesian> as rect -> GeometryToSharpVG.toRectangle rect |> Element.createWithStyle style
+
+    | :? BoundingBox2D<Meters, Cartesian> as bbox ->
+        GeometryToSharpVG.bboxToRectangle bbox |> Element.createWithStyle style
+
+    | :? Polygon2D<Meters, Cartesian> as polygon -> GeometryToSharpVG.toPolygon polygon |> Element.createWithStyle style
+
+    | _ ->
+        failwith (
+            "Unable to create SVG from geometry.\n"
+            + " This can be caused by not using the Cartesian coordinate system:\n"
+            + $"{geometry}"
+        )
--- test/PenPlotter.Tests/Plotter.fs
+++ test/PenPlotter.Tests/Plotter.fs
@@ -19,6 +19,17 @@ let testGeometry (geom: IGeometry) (expected: string) : unit =
     Assert.AreEqual(expected, actual)
 
 [<Test>]
+let ``Line2D to SVG`` () =
+    let expected: string =
+        """<line stroke="black" stroke-width="2" opacity="1" x1="0" y1="100" x2="100" y2="100"/>"""
+
+    let geom: Line2D<Meters, Cartesian> =
+        Line2D.through (Point2D.pixels 0. 100.) (Point2D.pixels 100. 100.)
+
+    testGeometry geom expected
+
+
+[<Test>]
 let ``Circle2D to SVG`` () =
     let expected: string =
         """<circle stroke="black" stroke-width="2" opacity="1" r="25" cx="50" cy="100"/>"""
@@ -29,11 +40,39 @@ let ``Circle2D to SVG`` () =
     testGeometry geom expected
 
 [<Test>]
-let ``Line2D to SVG`` () =
+let ``BoundingBox2D to SVG`` () =
     let expected: string =
-        """<line stroke="black" stroke-width="2" opacity="1" x1="0" y1="100" x2="100" y2="100"/>"""
+        """<rect stroke="black" stroke-width="2" opacity="1" x="0" y="50" width="150" height="50"/>"""
 
-    let geom: Line2D<Meters, Cartesian> =
-        Line2D.through (Point2D.pixels 0. 100.) (Point2D.pixels 100. 100.)
+    let geom: BoundingBox2D<Meters, Cartesian> =
+        BoundingBox2D.from (Point2D.pixels 0. 50.) (Point2D.pixels 150. 100.)
+
+    testGeometry geom expected
+
+[<Test>]
+let ``Rectangle2D to SVG`` () =
+    let expected: string =
+        """<rect stroke="black" stroke-width="2" opacity="1" x="25" y="50" width="50" height="100"/>"""
+
+    let geom: Rectangle2D<Meters, Cartesian> =
+            Rectangle2D.withDimensions
+                 (Size2D.create (Length.cssPixels 50) (Length.cssPixels 100))
+                 (Angle.degrees 0)
+                 (Point2D.pixels 50 100)
+
+    testGeometry geom expected
+    
+[<Test>]
+let ``Polygon2D outer circle to SVG`` () =
+    let expected: string =
+        """<polygon stroke="black" stroke-width="2" opacity="1" points="10,20 30,40 50,60 70,80"/>"""
+
+    let geom: Polygon2D<Meters, Cartesian> =
+        Polygon2D.singleLoop [
+            Point2D.pixels 10. 20.
+            Point2D.pixels 30. 40.
+            Point2D.pixels 50. 60.
+            Point2D.pixels 70. 80.
+        ]
 
     testGeometry geom expected
