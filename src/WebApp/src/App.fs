5ac90342190d0473e331333032654599bebea56f
Thomas Waters <twaters117@gmail.com>
2023-08-19 12:49:04 -0400

    Scaffolding of basic Fable/Feliz web application running off of webpack local server infrastructure

---
+++ src/WebApp/src/App.fs
@@ -0,0 +1,35 @@
+module App
+
+open Elmish
+open Elmish.React
+open Feliz
+
+// MODEL
+
+type Model = int
+
+type Msg =
+    | Increment
+    | Decrement
+
+let init () : Model = 0
+
+// UPDATE
+
+let update (msg: Msg) (model: Model) =
+    match msg with
+    | Increment -> model + 1
+    | Decrement -> model - 1
+
+// VIEW (rendered with React)
+
+let view (model: Model) dispatch =
+    Html.div [
+        Html.h1 "This is my Feliz-Fable App!"
+    ]
+
+// App
+Program.mkSimple init update view
+|> Program.withReactSynchronous "elmish-app"
+|> Program.withConsoleTrace
+|> Program.run
